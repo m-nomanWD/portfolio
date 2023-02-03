d55608d6458d3bf36ada85183ee7c58b1942877b
Muhammad Nauman <105644739+m-nomanWD@users.noreply.github.com>
2023-02-03 14:57:42 +0500

    Add files via upload

---
+++ assists/scripts/app.js
@@ -0,0 +1,265 @@
+// data for web
+const data = [
+  {
+    index: 0,
+    company: "PLASMOS",
+    about:
+      "Velit cupidatat commodo consequat culpa Lorem sint voluptate deserunt. Anim id do eiusmod nisi adipisicing Lorem qui do sunt fugiat aliqua est. Sint enim aute aliqua consequat qui sunt commodo elit et aute. Nostrud reprehenderit est culpa nostrud proident sint. Aliqua sit mollit magna eiusmod minim commodo est sit sint. Dolor exercitation non dolor commodo irure cillum dolor irure non proident sit ea aute veniam.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 1,
+    company: "NAXDIS",
+    about:
+      "Tempor occaecat ullamco sint duis. Ex eiusmod magna commodo mollit fugiat occaecat laborum ullamco do et veniam qui. Qui aute amet nostrud adipisicing enim. Laborum nisi labore id aliqua nostrud cillum incididunt.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 2,
+    company: "JIMBIES",
+    about:
+      "Aliqua exercitation et do laborum amet tempor adipisicing laboris minim ad quis minim. Commodo ipsum cillum voluptate culpa ipsum fugiat qui amet laborum dolore reprehenderit laborum officia. Deserunt quis Lorem commodo reprehenderit elit. Anim dolor qui est exercitation elit voluptate excepteur minim excepteur adipisicing. In occaecat esse fugiat fugiat consequat ullamco ut nisi aute nulla pariatur cupidatat. Fugiat et cupidatat nisi elit in consequat adipisicing. Fugiat mollit eiusmod ea sunt ullamco cillum.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 3,
+    company: "SCENTY",
+    about:
+      "Amet aliquip deserunt amet et Lorem do magna eiusmod. Irure in irure nostrud est laboris et adipisicing ad incididunt anim eiusmod qui laborum. Deserunt amet ipsum ea irure aliqua do culpa ea. Culpa ea tempor incididunt velit elit.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 4,
+    company: "AVIT",
+    about:
+      "Nostrud duis eiusmod reprehenderit elit mollit sunt. Eiusmod enim non sunt veniam esse aliqua deserunt ea dolor proident occaecat non occaecat esse. Dolore quis laboris cillum mollit esse amet Lorem fugiat Lorem.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 0,
+    company: "PLASMOS",
+    about:
+      "Velit cupidatat commodo consequat culpa Lorem sint voluptate deserunt. Anim id do eiusmod nisi adipisicing Lorem qui do sunt fugiat aliqua est. Sint enim aute aliqua consequat qui sunt commodo elit et aute. Nostrud reprehenderit est culpa nostrud proident sint. Aliqua sit mollit magna eiusmod minim commodo est sit sint. Dolor exercitation non dolor commodo irure cillum dolor irure non proident sit ea aute veniam.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 1,
+    company: "NAXDIS",
+    about:
+      "Tempor occaecat ullamco sint duis. Ex eiusmod magna commodo mollit fugiat occaecat laborum ullamco do et veniam qui. Qui aute amet nostrud adipisicing enim. Laborum nisi labore id aliqua nostrud cillum incididunt.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 2,
+    company: "JIMBIES",
+    about:
+      "Aliqua exercitation et do laborum amet tempor adipisicing laboris minim ad quis minim. Commodo ipsum cillum voluptate culpa ipsum fugiat qui amet laborum dolore reprehenderit laborum officia. Deserunt quis Lorem commodo reprehenderit elit. Anim dolor qui est exercitation elit voluptate excepteur minim excepteur adipisicing. In occaecat esse fugiat fugiat consequat ullamco ut nisi aute nulla pariatur cupidatat. Fugiat et cupidatat nisi elit in consequat adipisicing. Fugiat mollit eiusmod ea sunt ullamco cillum.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 3,
+    company: "SCENTY",
+    about:
+      "Amet aliquip deserunt amet et Lorem do magna eiusmod. Irure in irure nostrud est laboris et adipisicing ad incididunt anim eiusmod qui laborum. Deserunt amet ipsum ea irure aliqua do culpa ea. Culpa ea tempor incididunt velit elit.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+  {
+    index: 4,
+    company: "AVIT",
+    about:
+      "Nostrud duis eiusmod reprehenderit elit mollit sunt. Eiusmod enim non sunt veniam esse aliqua deserunt ea dolor proident occaecat non occaecat esse. Dolore quis laboris cillum mollit esse amet Lorem fugiat Lorem.\r\n",
+    viewSite: {
+      title: "vist site",
+      urlLink: "www.google.com",
+    },
+    github: {
+      title: "github",
+      urlLink: "www.github.com",
+    },
+    youtube: {
+      title: "youtube video",
+      urlLink: "www.youtube.com",
+    },
+  },
+];
+// dom objects const
+const projectDetail = document.querySelector(".project-info");
+const projectBtnContainer = document.querySelector(".project-btns");
+const proBtns = projectBtnContainer.querySelector("ul");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+// js variable/const
+let i = 0;
+let firstProject = data[i];
+const projectTabBtns = data.map((title) => {
+  return `<li class="btn-project" data-id = ${title.index}>${title.company}</li>`;
+});
+const innerHTMLProBtn = projectTabBtns;
+
+document.addEventListener("DOMContentLoaded", () => {
+  projectDetail.innerHTML = innerHtmlProDe;
+  proBtns.innerHTML = innerHTMLProBtn;
+  prevBtn.style.display = "none";
+});
+proBtns.addEventListener("click", function selectBtn(e) {
+  let index = parseInt(e.target.dataset.id);
+  // console.log(index);
+  i = index + i;
+
+  toggle();
+  i = 0;
+});
+nextBtn.addEventListener("click", nextProject);
+prevBtn.addEventListener("click", prevProject);
+
+// objects inner html
+
+let innerHtmlProDe = `<h2 class="project-title">${firstProject.company}</h2>
+            <div class="project-detail">
+              <p>
+               ${firstProject.about}
+              </p>
+              <div class="btn-container">
+                <ul>
+                  <li><a href="${firstProject.viewSite.urlLink}">${firstProject.viewSite.title}</a></li>
+                  <li><a href="${firstProject.github.urlLink}">${firstProject.github.title}</a></li>
+                  <li><a href="${firstProject.youtube.urlLink}">${firstProject.youtube.title}</a></li>
+                </ul>
+               
+              </div>
+            </div>
+         `;
+
+// functions
+function toggle() {
+  firstProject = data[i];
+  projectDetail.innerHTML = `<h2 class="project-title">${firstProject.company}</h2>
+            <div class="project-detail">
+              <p>
+               ${firstProject.about}
+              </p>
+              <div class="btn-container">
+                <ul>
+                  <li><a href="${firstProject.viewSite.urlLink}">${firstProject.viewSite.title}</a></li>
+                  <li><a href="${firstProject.github.urlLink}">${firstProject.github.title}</a></li>
+                  <li><a href="${firstProject.youtube.urlLink}">${firstProject.youtube.title}</a></li>
+                </ul>
+               
+            </div>
+         `;
+}
+function prevProject() {
+  if (i == 1) {
+    prevBtn.style.display = "none";
+  }
+  nextBtn.style.display = "block";
+  data[i--];
+  toggle();
+}
+function nextProject() {
+  if (i == data.length - 2) {
+    nextBtn.style.display = "none";
+  }
+  data[i++];
+  prevBtn.style.display = "block";
+  toggle();
+}
